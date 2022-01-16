7090572faee8a10f0a822dcd8cd122a136ea55a3
kryptopoo <kryptopoo@gmail.com>
2022-01-16 19:47:25 +0700

    restructure project

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const nftAnalyzer = require("./nft-analyzer");
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
   res.send("NFT Rarity app running...");
